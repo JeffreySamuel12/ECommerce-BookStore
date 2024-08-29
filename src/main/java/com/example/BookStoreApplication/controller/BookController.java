bc879914807c9adeb397c902bfe6a3682ddf46cc
Nihanth Kalisetti <Nihanth.Kalisetti@godigit.com>
2024-08-29 17:08:16 +0530

    Updated Book model

--- src/main/java/com/example/BookStoreApplication/controller/BookController.java
+++ src/main/java/com/example/BookStoreApplication/controller/BookController.java
@@ -5,10 +5,8 @@ import com.example.BookStoreApplication.model.Book;
 import com.example.BookStoreApplication.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/books")
@@ -25,21 +23,17 @@ public class BookController {
 
     @GetMapping("/all")
     public List<BookDTO> getAllBooks() {
-        List<Book> books = bookService.getAllBooks();
-        return books.stream().map(this::convertToDTO).collect(Collectors.toList());
+        return bookService.getAllBooks();
     }
 
     @GetMapping("/{id}")
     public BookDTO getBookById(@PathVariable Long id) {
-        Book book = bookService.getBookById(id).orElseThrow(() -> new RuntimeException("Book not found"));
-        return convertToDTO(book);
+        return bookService.getBookById(id);
     }
 
     @PutMapping("/update/{id}")
     public BookDTO updateBook(@PathVariable Long id, @RequestBody BookDTO bookDTO) {
-        Book bookDetails = convertToEntity(bookDTO);// Save the file name
-        Book updatedBook = bookService.updateBook(id, bookDetails);
-        return convertToDTO(updatedBook);
+        return bookService.updateBook(id, bookDTO);
     }
 
     @DeleteMapping("/delete/{id}")
@@ -49,38 +43,11 @@ public class BookController {
 
     @PutMapping("/changeQuantity/{id}/{quantity}")
     public BookDTO changeBookQuantity(@RequestHeader("Authorization") String token, @PathVariable long id, @PathVariable int quantity) {
-        Book updatedBook = bookService.changeBookQuantity(id, quantity);
-
-        return convertToDTO(updatedBook);
+        return bookService.changeBookQuantity(id, quantity);
     }
 
     @PutMapping("/changePrice/{id}/{price}")
     public BookDTO changeBookPrice(@RequestHeader("Authorization") String token, @PathVariable long id, @PathVariable double price) {
-        Book updatedBook = bookService.changeBookPrice(id, price);
-        return convertToDTO(updatedBook);
-    }
-
-    private BookDTO convertToDTO(Book book) {
-        return BookDTO.builder()
-                .id(book.getId())
-                .bookName(book.getBookName())
-                .bookAuthor(book.getBookAuthor())
-                .bookDescription(book.getBookDescription())
-                .bookLogo(book.getBookLogo())
-                .bookPrice(book.getBookPrice())
-                .bookQuantity(book.getBookQuantity())
-                .build();
-    }
-
-    private Book convertToEntity(BookDTO bookDTO) {
-        return Book.builder()
-                .id(bookDTO.getId())
-                .bookName(bookDTO.getBookName())
-                .bookAuthor(bookDTO.getBookAuthor())
-                .bookDescription(bookDTO.getBookDescription())
-                .bookLogo(bookDTO.getBookLogo())
-                .bookPrice(bookDTO.getBookPrice())
-                .bookQuantity(bookDTO.getBookQuantity())
-                .build();
+        return bookService.changeBookPrice(id, price);
     }
 }
