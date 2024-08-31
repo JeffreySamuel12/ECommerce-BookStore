07a64524bec9831942c7dd7dbb499bf5996be75d
Jeffrey.Samuel <Jeffrey.Samuel@godigit.com>
2024-08-31 13:19:17 +0530

    Updated with CrossOrigin Annotation

--- src/main/java/com/example/BookStoreApplication/controller/UserController.java
+++ src/main/java/com/example/BookStoreApplication/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.security.access.method.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
